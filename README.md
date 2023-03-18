dd191b5ec59f238dc94b7b8a58d7466bf9dbf03c
DeepOutSource <113112202+DeepOutSource@users.noreply.github.com>
2023-03-18 18:02:49 +0000

    rename README

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Coinc
+
+Program to seek coincidences from list-mode data. The program is greedy and fast.
+
+## Installation from sources
+
+See [installation instructions](INSTALL.md)
+
+## Dependencies
+
+None.
---
+++ coinc.c
@@ -0,0 +1,18 @@
+
+/*
+    Copyright (C) 2013-2020 Jaakko Julin <jaakko.julin@jyu.fi>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    See file COPYING for details.
+*/
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
